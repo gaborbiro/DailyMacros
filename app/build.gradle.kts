9fc36ae83e49476a819c12a9b196e8c7980c2558
Gabor Biro <gabor.biro@yahoo.com>
2025-08-28 20:21:03 +0100

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "dev.gaborbiro.dailymacros"
         minSdk = 31
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
