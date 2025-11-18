cf635035a2f529b6088371eb6d2b25c49c2a4d51
Gabor <gbiro@somnologymd.com>
2025-11-18 19:09:10 +0000

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.gaborbiro.dailymacros"
         minSdk = 31
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4.2"
+        versionCode = 15
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
