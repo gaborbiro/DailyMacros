c8de59d5691bb02bcd891dfd0483b967c99cbc2a
Gabor <gbiro@somnologymd.com>
2026-05-11 14:27:10 +0100

    Fix

--- app/src/main/kotlin/dev/gaborbiro/dailymacros/App.kt
+++ app/src/main/kotlin/dev/gaborbiro/dailymacros/App.kt
@@ -4,28 +4,43 @@ import android.app.Application
 import android.content.Context
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import androidx.work.WorkManager
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.components.SingletonComponent
 import dev.gaborbiro.dailymacros.data.db.AppDatabase
 import dev.gaborbiro.dailymacros.util.createNotificationChannels
-import javax.inject.Inject
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface AppWorkerFactoryEntryPoint {
+    fun hiltWorkerFactory(): HiltWorkerFactory
+}
 
 @HiltAndroidApp
 class App : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
 
     companion object {
         lateinit var appContext: Context
     }
 
     override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
+        get() {
+            val workerFactory = EntryPointAccessors.fromApplication(
+                this,
+                AppWorkerFactoryEntryPoint::class.java,
+            ).hiltWorkerFactory()
+
+            return Configuration.Builder()
+                .setWorkerFactory(workerFactory)
+                .build()
+        }
 
     override fun onCreate() {
         super.onCreate()
+        WorkManager.initialize(this, workManagerConfiguration)
         appContext = this
         AppDatabase.init(this)
         createNotificationChannels()
